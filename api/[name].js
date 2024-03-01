9a1ae11d6a21056a7d270ef7f451270bc4f4ac0f
白云苍狗 <admin@imalun.com>
2024-03-01 19:45:47 +0800

    🦄 refactor: 切换为 js

--- api/[name].js
+++ api/[name].js
@@ -1,12 +1,11 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import themify from '../lib/themify';
-import db from "../lib/db";
+import { themify } from '../lib/themify'
+import { setNumMulti, getNum } from "../lib/db"
 
 
 const PLACES = 7
-let __cache_counter: Record<string, number> = {}, shouldPush = true
+let __cache_counter = {}, shouldPush = true
 
-setInterval(() => { shouldPush = true }, 1000 * 60);
+setInterval(() => { shouldPush = true }, 1000 * 60)
 
 const pushDB = async () => {
     if (!shouldPush) return
@@ -21,20 +20,20 @@ const pushDB = async () => {
             }
         })
 
-        await db.setNumMulti(counters)
+        await setNumMulti(counters)
         __cache_counter = {}
     } catch (error) {
         console.log("pushDB is error: ", error)
     }
 }
 
-const getCountByName = async (name: string) => {
+const getCountByName = async (name) => {
     const defaultCount = { name, num: 0 }
-    if (name === 'demo') return { name, num: '0123456789' }
+    if (name === 'demo' || name === '') return { name, num: '0123456789' }
 
     try {
         if (!(name in __cache_counter)) {
-            const counter = await db.getNum(name) || defaultCount
+            const counter = await getNum(name) || defaultCount
             __cache_counter[name] = counter.num + 1
         } else {
             __cache_counter[name]++
@@ -48,21 +47,19 @@ const getCountByName = async (name: string) => {
 }
 
 
-export default async (req: VercelRequest, res: VercelResponse) => {
-    const { name, theme = 'moebooru' } = req.query
+export default async (req, res) => {
+    const { name = 'demo', theme = 'moebooru' } = req.query
+    const data = await getCountByName(name)
 
     let length = PLACES
-
-    res.setHeader('content-type', 'image/svg+xml')
-    res.setHeader('cache-control', 'max-age=0, no-cache, no-store, must-revalidate')
-
-    const data = await getCountByName(<string>name)
 
     if (name === 'demo') {
         res.setHeader('cache-control', 'max-age=31536000')
         length = 10
     }
 
-    const renderSvg = themify({ count: data.num, theme: <string>theme, length })
-    res.send(renderSvg)
+    res.setHeader('content-type', 'image/svg+xml')
+    res.setHeader('cache-control', 'max-age=0, no-cache, no-store, must-revalidate')
+    res.send(themify({ count: data.num, theme: theme, length }))
+    res.end()
 }
